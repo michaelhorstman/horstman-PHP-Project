ee133ae87567d9025705eb8fc903c5778ab29247
michaelhorstman <gmichael98@gmail.com>
2016-03-03 22:42:59 -0500

    Update config.php

--- htdocs/config.php
+++ htdocs/config.php
@@ -2,7 +2,7 @@
 
 define("DB_NAME", "googlefibersupporters");
 	define("DB_USER", "google-fiber-supporters-michael");
-	define("DB_PASS", "baseball30=");
+	define("DB_PASS", "password");
 	define("DB_HOST", "localhost");
 	define("DB_PORT", "3306");
 
